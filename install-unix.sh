31cb72b95ee6053a8555211618b073dd7dd959e0
Oleg Karasik <karasik.oleg.nikolaevich@gmail.com>
2026-05-09 10:49:28 +0300

    Update install-unix.sh

--- install-unix.sh
+++ install-unix.sh
@@ -10,23 +10,27 @@ if [ ! -d "$HOME/.vim/after/syntax" ]; then
   mkdir -p "$HOME/.vim/after/syntax"
 fi
 
+# Source script directory
+#
+SOURCE=$(dirname "$0")
+
 # Link files from the repository
 #
 if [ ! -f "$HOME/.vim/after/ftplugin/cpp.vim" ]; then
-  ln -s ".vim/after/ftplugin/cpp.vim" "$HOME/.vim/after/ftplugin/cpp.vim"
+  ln -sf "$SOURCE/.vim/after/ftplugin/cpp.vim" "$HOME/.vim/after/ftplugin/cpp.vim"
 fi
 if [ ! -f "$HOME/.vim/after/ftplugin/markdown.vim" ]; then
-  ln -s ".vim/after/ftplugin/markdown.vim" "$HOME/.vim/after/ftplugin/markdown.vim"
+  ln -sf "$SOURCE/.vim/after/ftplugin/markdown.vim" "$HOME/.vim/after/ftplugin/markdown.vim"
 fi
 if [ ! -f "$HOME/.vim/after/ftplugin/programming.vim" ]; then
-  ln -s ".vim/after/ftplugin/programming.vim" "$HOME/.vim/after/ftplugin/programming.vim"
+  ln -sf "$SOURCE/.vim/after/ftplugin/programming.vim" "$HOME/.vim/after/ftplugin/programming.vim"
 fi
 if [ ! -f "$HOME/.vim/after/syntax/markdown.vim" ]; then
-  ln -s ".vim/after/syntax/markdown.vim" "$HOME/.vim/after/syntax/markdown.vim"
+  ln -sf "$SOURCE/.vim/after/syntax/markdown.vim" "$HOME/.vim/after/syntax/markdown.vim"
 fi
 if [ ! -f "$HOME/.vim/coc-settings.json" ]; then
-  ln -s ".vim/coc-settings.json" "$HOME/.vim/coc-settings.json"
+  ln -sf "$SOURCE/.vim/coc-settings.json" "$HOME/.vim/coc-settings.json"
 fi
 if [ ! -f "$HOME/.vimrc" ]; then
-  ln -s ".vimrc" "$HOME/.vimrc"
+  ln -sf "$SOURCE/.vimrc" "$HOME/.vimrc"
 fi
