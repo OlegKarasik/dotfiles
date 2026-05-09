8f45fed6377585427edf7631a2081e961e4ceaac
Oleg Karasik <karasik.oleg.nikolaevich@gmail.com>
2026-05-09 11:06:11 +0300

    Update install-windows.ps1

--- install-windows.ps1
+++ install-windows.ps1
@@ -1,33 +1,33 @@
 # Create directories
-#%USERPROFILE%\vimfiles
-if [ ! -d "$HOME/.vim/after/ftplugin" ]; then
-  mkdir -p "$HOME/.vim/after/ftplugin"
-fi
-if [ ! -d "$HOME/.vim/after/syntax" ]; then
-  mkdir -p "$HOME/.vim/after/syntax"
-fi
+#
+if (-not (Test-Path -Path "%USERPROFILE%\vimfiles\after\ftplugin")) {
+  New-Item -Path "%USERPROFILE%\vimfiles\after\ftplugin" -ItemType Directory;
+}
+if (-not (Test-Path -Path "%USERPROFILE%\vimfiles\after\syntax")) {
+  New-Item -Path "%USERPROFILE%\vimfiles\after\syntax" -ItemType Directory;
+}
 
 # Source script directory
 #
-SOURCE=$(dirname "$0")
+$SOURCE=$PSScriptRoot
 
 # Link files from the repository
 #
-if [ ! -f "$HOME/.vim/after/ftplugin/cpp.vim" ]; then
-  ln -sf "$SOURCE/.vim/after/ftplugin/cpp.vim" "$HOME/.vim/after/ftplugin/cpp.vim"
-fi
-if [ ! -f "$HOME/.vim/after/ftplugin/markdown.vim" ]; then
-  ln -sf "$SOURCE/.vim/after/ftplugin/markdown.vim" "$HOME/.vim/after/ftplugin/markdown.vim"
-fi
-if [ ! -f "$HOME/.vim/after/ftplugin/programming.vim" ]; then
-  ln -sf "$SOURCE/.vim/after/ftplugin/programming.vim" "$HOME/.vim/after/ftplugin/programming.vim"
-fi
-if [ ! -f "$HOME/.vim/after/syntax/markdown.vim" ]; then
-  ln -sf "$SOURCE/.vim/after/syntax/markdown.vim" "$HOME/.vim/after/syntax/markdown.vim"
-fi
-if [ ! -f "$HOME/.vim/coc-settings.json" ]; then
-  ln -sf "$SOURCE/.vim/coc-settings.json" "$HOME/.vim/coc-settings.json"
-fi
-if [ ! -f "$HOME/.vimrc" ]; then
-  ln -sf "$SOURCE/.vimrc" "$HOME/.vimrc"
-fi
+if (-not (Test-Path -Path "%USERPROFILE%\vimfiles\after\ftplugin\cpp.vim" -Leaf)) {
+  New-Item -Value "$SOURCE\.vim\after\ftplugin\cpp.vim" -Path "%USERPROFILE%\vimfiles\after\ftplugin\cpp.vim" -ItemType HardLink;
+}
+if (-not (Test-Path -Path "%USERPROFILE%\vimfiles\after\ftplugin\markdown.vim" -Leaf)) {
+  New-Item -Value "$SOURCE\.vim\after\ftplugin\markdown.vim" -Path "%USERPROFILE%\vimfiles\after\ftplugin\markdown.vim" -ItemType HardLink;
+}
+if (-not (Test-Path -Path "%USERPROFILE%\vimfiles\after\ftplugin\programming.vim" -Leaf)) {
+  New-Item -Value "$SOURCE\.vim\after\ftplugin\programming.vim" -Path "%USERPROFILE%\vimfiles\after\ftplugin\programming.vim" -ItemType HardLink;
+}
+if (-not (Test-Path -Path "%USERPROFILE%\vimfiles\after\syntax\markdown.vim" -Leaf)) {
+  New-Item -Value "$SOURCE\.vim\after\syntax\markdown.vim" -Path "%USERPROFILE%\vimfiles\after\syntax\markdown.vim" -ItemType HardLink;
+}
+if (-not (Test-Path -Path "%USERPROFILE%\vimfiles\coc-settings.json" -Leaf)) {
+  New-Item -Value "$SOURCE\.vim\coc-settings.json" -Path "%USERPROFILE%\vimfiles\coc-settings.json" -ItemType HardLink;
+}
+if (-not (Test-Path -Path "%USERPROFILE%\vimfiles\_vimrc" -Leaf)) {
+  New-Item -Value "$SOURCE\.vimrc" -Path "%USERPROFILE%\vimfiles\_vimrc" -ItemType HardLink;
+}
